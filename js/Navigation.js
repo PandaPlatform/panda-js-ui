9f636a9fecf403c4a131471757d67f4587b169ec
Ioannis Papikas <papikas.ioan@gmail.com>
2016-11-20 13:43:56 +0100

    Fix and add extra listeners in Navigation.js

--- js/Navigation.js
+++ js/Navigation.js
@@ -2,7 +2,7 @@ var Panda = Panda || {};
 Panda.Ui = Panda.Ui || {};
 
 (function ($) {
-    $(document).one('ready', function () {
+    $(document).one("ready", function () {
         // Init Navigation handlers
         Panda.Ui.Navigation.init();
     });
@@ -20,6 +20,9 @@ Panda.Ui = Panda.Ui || {};
                     Panda.Ui.Navigation.triggerClick($(this));
                 });
             });
+
+            // Trigger content modified to select items
+            $(document).trigger('panda.content.modified');
         },
         triggerClick: function (navItem) {
             // Get whether this item has the selected class
@@ -31,29 +34,29 @@ Panda.Ui = Panda.Ui || {};
             var thisNavGroup = navItem.data('static-nav').navgroup;
 
             // Clear all selected from navigation with the same targetgroup
-            $('[data-static-nav]').each(function () {
-                if (typeof($(this).data('static-nav').navgroup) != 'undefined' && $(this).data('static-nav').navgroup == thisNavGroup) {
+            $("[data-static-nav]").each(function () {
+                if (typeof($(this).data('static-nav').navgroup) != "undefined" && $(this).data('static-nav').navgroup == thisNavGroup) {
                     $(this).removeClass('selected');
                 }
             });
 
             // Set this item as selected
-            if (!(selected && navItem.data('static-nav').display == 'toggle')) {
+            if (!(selected && navItem.data("static-nav").display == "toggle")) {
                 navItem.addClass('selected');
             }
 
-            if (navItem.data('static-nav').display == 'none' || navItem.data('static-nav').display == 'toggle') {
-                // Clear All content from target container from the same group if 'clearAll'
-                $("[data-targetgroupid='" + thisTargetGroup + "']", '#' + thisTargetContainer).not('.noDisplay').addClass('noDisplay');
-            } else if (navItem.data('static-nav').display == 'all') {
-                // Display All content from target container from the same group if 'displayAll'
-                $("[data-targetgroupid ='" + thisTargetGroup + "']", '#' + thisTargetContainer).removeClass('noDisplay');
+            if (navItem.data("static-nav").display == "none" || navItem.data("static-nav").display == "toggle") {
+                // Clear All content from target container from the same group if "clearAll"
+                $("[data-targetgroupid='" + thisTargetGroup + "']", "#" + thisTargetContainer).not(".noDisplay").addClass("noDisplay");
+            } else if (navItem.data("static-nav").display == "all") {
+                // Display All content from target container from the same group if "displayAll"
+                $("[data-targetgroupid ='" + thisTargetGroup + "']", "#" + thisTargetContainer).removeClass("noDisplay");
             }
 
             // Set Display to dataRef
             var thisTarget = navItem.data('static-nav').ref;
-            if (!(selected && navItem.data('static-nav').display == 'toggle')) {
-                $('#' + thisTarget, '#' + thisTargetContainer).removeClass('noDisplay');
+            if (!(selected && navItem.data("static-nav").display == "toggle")) {
+                $("#" + thisTarget, "#" + thisTargetContainer).removeClass("noDisplay");
             }
         }
     };
